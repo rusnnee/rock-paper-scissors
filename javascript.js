dc5290fde6dac733d2e17955dcdfcc933de69130
rusnnee <36538819+rusnnee@users.noreply.github.com>
2026-02-20 18:56:31 +0000

    add basic functionality to javascript.js (no ending)

--- javascript.js
+++ javascript.js
@@ -1,12 +1,25 @@
 let humanScore = 0;
 let computerScore = 0; 
 
+const buttons = document.querySelectorAll("button");
 
-while (humanScore < 3 && computerScore < 3){
-    console.log(playRound(getComputerChoice(), getHumanChoice()));
-    console.log (computerScore + "(c) vs (h) " + humanScore); 
-}
-console.log("GAME OVER " + (computerScore > humanScore ? "computer" : "human") + " wins");
+buttons.forEach((button)=>{
+    switch (button.className){
+        case "rock":
+            button.addEventListener("click", () => playRound ("r", getComputerChoice()))
+            break;
+
+        case "paper":
+            button.addEventListener("click", () => playRound ("p", getComputerChoice()))
+            break;
+
+        case "scissors":
+            button.addEventListener("click", () => playRound ("s", getComputerChoice()))
+            break;
+    }
+})
+
+const scores = document.querySelectorAll('.score');
 
 function getComputerChoice(){
     let num = Math.random();
@@ -20,15 +33,31 @@ function getHumanChoice (){
     return prompt ("your choice").toLowerCase();
 }
 
-function playRound(computerChoice, humanChoice){
+function playRound(humanChoice, computerChoice){
     console.log(computerChoice + "(c) vc {h)"  + humanChoice);
 
     let ans; 
     (computerChoice[0] == "s" && humanChoice[0] == "p") ||
     (computerChoice[0] == "p" && humanChoice[0] == "r") ||
     (computerChoice[0] == "r" && humanChoice[0] == "s") ? (++computerScore,  ans = "computer wins" ) : 
+    computerChoice[0] == humanChoice[0] ? ans = "draw" : (ans = "human win", ++humanScore);
     
-    computerChoice[0] == humanChoice[0] ? ans = "draw" : (ans = "you win", ++humanScore);
+    updateScores();
+    
+    console.log( ans); 
+    console.log()
+    console.log(computerScore + "(c) vc {h)"  + humanScore);
+}
 
-    return ans; 
+function updateScores(){
+    scores.forEach((score) =>{
+        switch (score.parentNode.id){
+            case "human": 
+                score.textContent = humanScore;
+                break;
+            case "computer":
+                score.textContent = computerScore; 
+                break;
+        }
+    })
 }
