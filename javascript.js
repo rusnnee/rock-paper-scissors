2abd0f5372606639c553a26e50660eda5205a493
rusnnee <36538819+rusnnee@users.noreply.github.com>
2026-02-23 12:42:39 +0000

    Add new game logic to javascript.js and index.html

--- javascript.js
+++ javascript.js
@@ -1,7 +1,18 @@
 let humanScore = 0;
 let computerScore = 0; 
+const maxScore = 5;
 
+const scores = document.querySelectorAll('.score');
 const buttons = document.querySelectorAll("button");
+const computerChoiceText = document.querySelector(".computer-choice"); 
+const message = document.querySelector('.message')
+const btnNewGame = document.querySelector('.new-game')
+btnNewGame.style.visibility = 'hidden'; 
+
+btnNewGame.addEventListener("click", () => {
+    resetGame();
+    if (btnNewGame.style.visibility == 'visible') btnNewGame.style.visibility = 'hidden'; 
+})
 
 buttons.forEach((button)=>{
     switch (button.className){
@@ -19,34 +30,40 @@ buttons.forEach((button)=>{
     }
 })
 
-const scores = document.querySelectorAll('.score');
+resetGame(); 
 
 function getComputerChoice(){
-    let num = Math.random();
+    const num = Math.random();
 
-    return num < (1/3) ? "rock" :
-    num < 2/3 ? "paper":
-    "scissors";
+    let text; 
+
+    num < (1/3) ? text = "rock" :
+    num < 2/3 ? text = "paper":
+    text = "scissors";
+
+    updateComputerChoiceText(text);
+    return text; 
 }
 
-function getHumanChoice (){
-    return prompt ("your choice").toLowerCase();
-}
 
 function playRound(humanChoice, computerChoice){
-    console.log(computerChoice + "(c) vc {h)"  + humanChoice);
 
-    let ans; 
     (computerChoice[0] == "s" && humanChoice[0] == "p") ||
     (computerChoice[0] == "p" && humanChoice[0] == "r") ||
-    (computerChoice[0] == "r" && humanChoice[0] == "s") ? (++computerScore,  ans = "computer wins" ) : 
-    computerChoice[0] == humanChoice[0] ? ans = "draw" : (ans = "human win", ++humanScore);
+    (computerChoice[0] == "r" && humanChoice[0] == "s") ? (++computerScore, updateMessage('Computer scores')) : 
+    computerChoice[0] == humanChoice[0] ? (updateMessage('Draw')) : 
+    (++humanScore, updateMessage('You score')); 
     
     updateScores();
-    
-    console.log( ans); 
-    console.log()
-    console.log(computerScore + "(c) vc {h)"  + humanScore);
+    if (computerScore >= maxScore || humanScore >= maxScore) finishGame(); 
+}
+
+function updateComputerChoiceText(text){
+    computerChoiceText.textContent = text;
+}
+
+function updateMessage(text){
+    message.textContent = text; 
 }
 
 function updateScores(){
@@ -61,3 +78,22 @@ function updateScores(){
         }
     })
 }
+
+function finishGame(){
+    computerScore > humanScore ? updateMessage ("COMPUTER WINS")
+    : updateMessage("YOU WIN");
+
+    console.log (btnNewGame.style.visibility)
+    btnNewGame.style.visibility = 'visible'; 
+    console.log (btnNewGame.style.visibility)
+}
+
+function resetGame(){
+    console.log ("reset game")
+
+    humanScore = 0; 
+    computerScore = 0; 
+    updateComputerChoiceText("Computer's choice")
+    updateMessage("New game has started. Make your choice");
+}
+
